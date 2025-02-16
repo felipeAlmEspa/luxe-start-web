2f95e452b2a1fd61099d09da5553e7893af45706
Tomas Felipe <97459312+felipeAlmEspa@users.noreply.github.com>
2025-02-16 18:05:57 -0500

    correcones

--- helper/useRefetchOnFocus.js
+++
@@ -1,9 +0,0 @@
-import { useFocusEffect } from "@react-navigation/native";
-
-const useRefetchOnFocus = (refetch) => {
-  useFocusEffect(() => {
-    refetch();
-  });
-  /* Maybe subscribe to App state here too */
-};
-export default useRefetchOnFocus;
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Exporta como HTML estático
+  basePath: "/luxe-prod", // Reemplaza con el nombre de tu repo en GitHub
   images: {
+    unoptimized: true, // Evita la optimización de imágenes en GitHub Pages
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "raw.githubusercontent.com", // El dominio que usas
-        pathname: "/felipeAlmEspa/luxeApi/main/assets/**", // La ruta del archivo en GitHub
+        hostname: "raw.githubusercontent.com",
+        pathname: "/felipeAlmEspa/luxeApi/main/assets/**",
       },
     ],
   },
